c1f8ba0ce04c9670ffc8a8f89e1ef22b1d18e1ca
RaghadDeveloper <raghadmh620@gmail.com>
2025-07-28 09:09:55 +0300

    Edit comment style

--- src/components/LessonComments/LessonComments.jsx
+++ src/components/LessonComments/LessonComments.jsx
@@ -1,5 +1,7 @@
 import "./LessonComments.css";
 import img from "./../../assets/images/profileImg.jpg";
+import { AiOutlineLike } from "react-icons/ai";
+import Comment from "../Comment/Comment";
 
 function LessonComments() {
   return (
@@ -7,12 +9,69 @@ function LessonComments() {
       <h2>Comments:</h2>
       <div className="comment">
         <img className="user-img" src={img} />
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur,
-          dolorem iste. Ea odit natus vel reiciendis similique quibusdam non
-          maxime fugit repellendus dicta. Eos iusto quisquam dolorum autem.
-          Eveniet, rem.
-        </p>
+        <div className="user-comment">
+          <p>
+            <h4>user name</h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur,
+            dolorem iste. Ea odit natus vel reiciendis similique quibusdam non
+            maxime fugit repellendus dicta. Eos iusto quisquam dolorum autem.
+            Eveniet, rem.
+          </p>
+          <div className="actions">
+            <div>
+              <span>Like</span>
+              <span>Reply</span>
+            </div>
+            <div>
+              16
+              <AiOutlineLike />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="comment reply">
+        <img className="user-img" src={img} />
+        <div className="user-comment">
+          <p>
+            <h4>user name</h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur,
+            dolorem iste. Ea odit natus vel reiciendis similique quibusdam non
+            maxime fugit repellendus dicta. Eos iusto quisquam dolorum autem.
+            Eveniet, rem.
+          </p>
+          <div className="actions">
+            <div>
+              <span>Like</span>
+              <span>Reply</span>
+            </div>
+            <div>
+              16
+              <AiOutlineLike />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="comment">
+        <img className="user-img" src={img} />
+        <div className="user-comment">
+          <p>
+            <h4>user name</h4>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur,
+            dolorem iste. Ea odit natus vel reiciendis similique quibusdam non
+            maxime fugit repellendus dicta. Eos iusto quisquam dolorum autem.
+            Eveniet, rem.
+          </p>
+          <div className="actions">
+            <div>
+              <span>Like</span>
+              <span>Reply</span>
+            </div>
+            <div>
+              16
+              <AiOutlineLike />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
