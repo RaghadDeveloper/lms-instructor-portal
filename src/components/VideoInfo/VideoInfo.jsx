a4d438562c0650a08672d5484d5e8600ad0427a5
RaghadDeveloper <raghadmh620@gmail.com>
2025-08-26 18:08:53 +0300

    Added fetch lessons views functionality

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -2,6 +2,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import "./VideoInfo.css";
 import { FaRegEdit } from "react-icons/fa";
 import InfoBlock from "../InfoBlock/InfoBlock";
+import { useEffect, useState } from "react";
+import { getAllProfiles } from "../../features/users/usersThunk";
+import { useDispatch } from "react-redux";
+import ProfilesList from "../ProfilesList/ProfilesList";
 
 function formatTime(timeStr) {
   const [hours, minutes] = timeStr.split(":").map(Number);
@@ -11,45 +15,72 @@ function formatTime(timeStr) {
 }
 
 function VideoInfo({ lesson, onCommentsClick }) {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [showProfilesList, setShowProfilesList] = useState(false);
   const { lessonId, courseId } = useParams();
 
+  useEffect(() => {
+    if (showProfilesList) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showProfilesList]);
+
+  const handleGetViews = async () => {
+    if (!lesson?.views_count) return;
+    setShowProfilesList(true);
+    await dispatch(getAllProfiles({ views_lesson_id: lessonId }));
+  };
+
   return (
-    <div className="video-info card">
-      <video poster={lesson?.image_url} controls>
-        <source src={lesson?.video_url} type="video/mp4" />
-      </video>
+    <>
+      <div className="video-info card">
+        <video poster={lesson?.image_url} controls>
+          <source src={lesson?.video_url} type="video/mp4" />
+        </video>
 
-      <div className="header">
-        <h2>
-          <span>01. </span>
-          {lesson?.title}
-        </h2>
-        <button
-          className="edit-btn"
-          onClick={() =>
-            navigate(`/courses/${courseId}/lesson/edit/${lessonId}`)
-          }
-        >
-          <FaRegEdit />
-        </button>
-      </div>
+        <div className="header">
+          <h2>
+            <span>01. </span>
+            {lesson?.title}
+          </h2>
+          <button
+            className="edit-btn"
+            onClick={() =>
+              navigate(`/courses/${courseId}/lesson/edit/${lessonId}`)
+            }
+          >
+            <FaRegEdit />
+          </button>
+        </div>
 
-      <p>{lesson?.description}</p>
-      <div className="row">
-        <InfoBlock
-          label={"Duration"}
-          value={lesson && formatTime(lesson?.video_duration)}
-        />
-        <InfoBlock label={"views"} value={lesson?.views_count} />
-        <InfoBlock label={"Likes"} value={lesson?.likes_count} />
-        <InfoBlock
-          label={"Comments"}
-          value={lesson?.comment_count}
-          onClick={onCommentsClick}
-        />
+        <p>{lesson?.description}</p>
+        <div className="row">
+          <InfoBlock
+            label={"Duration"}
+            value={lesson && formatTime(lesson?.video_duration)}
+          />
+          <InfoBlock
+            label={"views"}
+            value={lesson?.views_count}
+            onClick={handleGetViews}
+          />
+          <InfoBlock label={"Likes"} value={lesson?.likes_count} />
+          <InfoBlock
+            label={"Comments"}
+            value={lesson?.comment_count}
+            onClick={onCommentsClick}
+          />
+        </div>
       </div>
-    </div>
+      {showProfilesList && <ProfilesList setIsShow={setShowProfilesList} />}
+    </>
   );
 }
 
